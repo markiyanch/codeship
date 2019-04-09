b162f90eab5d26c8054c1937eb0cfe0c6bb60ae6
markiyanch <markiyanch@gmail.com>
2019-04-09 10:39:55 +0300

    convert.py

---
+++ convert.py
@@ -0,0 +1,9 @@
+import os
+
+os.system('pip install -r requirements.txt')
+#!/usr/bin/env python
+Celsius = int(raw_input("Enter a temperature in Celsius: "))
+
+Fahrenheit = 9.0/5.0 * Celsius + 32
+
+print "Temperature:", Celsius, "Celsius = ", Fahrenheit, " F"
