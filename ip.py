48a19760e645dbc108256f35b07621a117a1aa48
markiyanch <markiyanch@gmail.com>
2019-04-09 10:59:27 +0300

    ip.py

---
+++ ip.py
@@ -0,0 +1,17 @@
+import urllib
+import re
+import os
+
+os.system('pip install -r requirements.txt')
+
+print "we will try to open this url, in order to get IP Address"
+
+url = "http://checkip.dyndns.org"
+
+print url
+
+request = urllib.urlopen(url).read()
+
+theIP = re.findall(r"d{1,3}.d{1,3}.d{1,3}.d{1,3}", request)
+
+print "your IP Address is: ",  theIP
