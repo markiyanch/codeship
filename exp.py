6697472a2e7d2c6f6ed349dbfa9ce4351791c8ad
markiyanch <markiyanch@gmail.com>
2019-04-09 11:10:00 +0300

    exp.py

---
+++ exp.py
@@ -0,0 +1,24 @@
+
+# import complex math module
+import cmath
+import os
+
+os.system('pip install -r requirements.txt')
+
+a = 1
+b = 5
+c = 6
+
+# To take coefficient input from the users
+# a = float(input('Enter a: '))
+# b = float(input('Enter b: '))
+# c = float(input('Enter c: '))
+
+# calculate the discriminant
+d = (b**2) - (4*a*c)
+
+# find two solutions
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
+
+print('The solution are {0} and {1}'.format(sol1,sol2))
