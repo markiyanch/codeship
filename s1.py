2ef75e95cb23e18dab59f8e58f8d72069e706dd6
markiyanch <markiyanch@gmail.com>
2019-04-09 11:11:56 +0300

    s1.py

---
+++ s1.py
@@ -0,0 +1,22 @@
+import os
+
+os.system('pip install -r requirements.txt')
+
+def recur_sum(n):
+   """Function to return the sum
+   of natural numbers using recursion"""
+   if n <= 1:
+       return n
+   else:
+       return n + recur_sum(n-1)
+
+# change this value for a different result
+num = 16
+
+# uncomment to take input from the user
+#num = int(input("Enter a number: "))
+
+if num < 0:
+   print("Enter a positive number")
+else:
+   print("The sum is",recur_sum(num))
